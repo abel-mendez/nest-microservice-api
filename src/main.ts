9dac5ba75ae9b48eb2046fe6ecf3dd479c6e7285
abel-mendez <abel.m.villagra@gmail.com>
2024-09-20 08:31:36 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
+
   const microservice =
     await NestFactory.createMicroservice<MicroserviceOptions>(BusinessModule, {
       transport: Transport.TCP,
